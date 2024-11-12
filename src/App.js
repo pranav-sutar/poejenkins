8051e88229926a83b87b38c3d68505e812e8fba3
Pranav Sutar <pranavssutar@gmail.com>
2024-11-12 13:46:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const link = "https://github.com/pranav-sutar";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to react app</h2>
+      <p>App is propperly working</p>
+      <a href={link} target='_blank' >@pranavSutar ©PranavSutar</a>
     </div>
   );
 }
